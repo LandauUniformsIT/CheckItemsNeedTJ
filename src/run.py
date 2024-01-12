53dd7e2ac9057b2d59c6957fade240b6cf10ce49
javiermartinez10 <85707310+javiermartinez10@users.noreply.github.com>
2024-01-12 11:34:51 -0500

    Changes required

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+import numpy as np
 from utils import *
 
 def main(argv):
@@ -16,7 +17,7 @@ def main(argv):
         df_zeroes = df_items[(df_items['STL_QTY'] <= 0.0) & (df_items['STL_L_QTY'] <= 0.0) & (df_items['OB_QTY'] <= 0.0) & (df_items['OB_L_QTY'] <= 0.0)]
         df_zeroes.to_excel('Items Zero qty.xlsx', index=False)
         
-        df_stl_l = get_transfers_for_wh(df_items,'STL_L_QTY', 'STL-L', 'STL')
+        df_stl_l = get_transfers_for_wh2(df_items,'STL_L_QTY', 'STL-L', 'STL')
         df_ob = get_transfers_for_wh(df_items,'OB_QTY', 'OB', 'STL-L')
         df_ob_l = get_transfers_for_wh(df_items,'OB_L_QTY', 'OB-L', 'STL-L')
         df_transfers = pd.concat([df_stl_l, df_ob, df_ob_l])
@@ -30,7 +31,7 @@ def main(argv):
         df_need_2.to_excel('Items Need TJ.xlsx', index=False)
 
 def get_transfers_for_wh(df_items, column, wh_name, to_wh_name):
-    df = df_items[df_items[column] > 0]
+    df = df_items[df_items[column] > 0].copy()
     columns = ['Item', 'Color', 'Size', 'Qty', 'STL_QTY', 'STL_L_QTY', 'OB_QTY', 'OB_L_QTY', 'Total_Avail']
     columns_final = ['FromSite', 'FromWh', 'Item', 'Color', 'Size', 'Qty', 'ToSite', 'ToWh']
     df.columns = columns
@@ -38,7 +39,22 @@ def get_transfers_for_wh(df_items, column, wh_name, to_wh_name):
     df['FromWh'] = wh_name
     df['ToSite'] = 'KT'
     df['ToWh'] = to_wh_name
-    df['Qty'] = df_items[column]
+    df['Qty'] = df[column]
+    df = df[columns_final]
+    return df
+
+def get_transfers_for_wh2(df_items, column, wh_name, to_wh_name):
+    df = df_items.copy()
+    columns = ['Item', 'Color', 'Size', 'Qty', 'STL_QTY', 'STL_L_QTY', 'OB_QTY', 'OB_L_QTY', 'Total_Avail']
+    columns_final = ['FromSite', 'FromWh', 'Item', 'Color', 'Size', 'Qty', 'ToSite', 'ToWh']
+    df.columns = columns
+    df['Needed'] = df['Qty'] - df['STL_QTY']
+    df = df[df['Needed'] > 0]
+    df['FromSite'] = 'KT'
+    df['FromWh'] = wh_name
+    df['ToSite'] = 'KT'
+    df['ToWh'] = to_wh_name
+    df['Qty'] = np.where(df['Needed'] < df['STL_QTY'], df['Needed'], df['STL_QTY'])
     df = df[columns_final]
     return df
 
